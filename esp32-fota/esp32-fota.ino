86bc828818b916ba268e3dd4e811127318440703
Chris Joyce <chrisj@benon.com>
2018-12-09 11:07:49 +1000

    Moved execHTTPcheck it own file

--- esp32-fota/esp32-fota.ino
+++ esp32-fota/esp32-fota.ino
@@ -20,6 +20,7 @@
 
 #include "local.h"
 #include "execOTA.h"
+#include "execHTTPcheck.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "ArduinoJson.h"
@@ -34,14 +35,6 @@
 const String firwmaretype = "esp32-fota-http";
 const int firwmareversion = 1;
 
-
-WiFiClient client;
-// Variables to validate
-// response from webserver
-
-String host ;
-String bin ;
-int port = 80;
 
 const int led = 21;
 uint64_t chipid;  
@@ -75,80 +68,6 @@ void setup_wifi() {
 }
 
 
-void httpget() {
-
-   Serial.println("Getting HTTP");
-   Serial.println("----------------------");
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
- 
-    HTTPClient http;
- 
-    http.begin(firwmareupdate); //Specify the URL
-    int httpCode = http.GET();                                        //Make the request
- 
-    if (httpCode == 200 ) { //Check is a file was returned
- 
-        String payload = http.getString();
-
-        int str_len = payload.length() + 1; 
-        char JSONMessage[str_len];
-        payload.toCharArray(JSONMessage, str_len);
-        
-        StaticJsonBuffer<300> JSONBuffer;                         //Memory pool
-        JsonObject& parsed = JSONBuffer.parseObject(JSONMessage); //Parse message
-        
-        if (!parsed.success()) {   //Check for errors in parsing
-          Serial.println("Parsing failed");
-          delay(5000);
-          return;
-        }
-        
-        const char * pltype = parsed["type"];
-        int plversion = parsed["version"];
-        const char * plhost = parsed["host"];
-        port = parsed["port"];
-        const char * plbin = parsed["bin"];
-
-        String jshost(plhost);
-        String jsbin(plbin);
-
-        host = jshost;
-        bin = jsbin;
-        
-        String fwtype(pltype);
-        
-        if ( plversion > firwmareversion && fwtype == firwmaretype ) {
-          Serial.println("update needed");
-          Serial.print("Firmware type: ");
-          Serial.println(pltype);
-          Serial.print("version: ");
-          Serial.println(plversion);
-          
-          Serial.print("host : ");
-          Serial.println(host);
-          Serial.print("port : ");
-          Serial.println(port);
-          Serial.print("bin : ");
-          Serial.println(bin);
-
-          execOTA(client, host ,bin ,port);
-        } else {
-          Serial.println("no update needed");
-        }
-        
-      }
- 
-    else {
-      Serial.println("Error on HTTP request");
-    }
- 
-    http.end(); //Free the resources
-  }
- 
-  Serial.println("----------------------");
- 
-}
-
 
 void loop() {
   digitalWrite(led, HIGH);
@@ -165,7 +84,7 @@ void loop() {
 	Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
 
 	delay(1500);
-  httpget();
+  execHTTPcheck(firwmaretype , firwmareversion);
   digitalWrite(led, LOW);
   delay(2000);
 }
---
+++ esp32-fota/execHTTPcheck.h
@@ -0,0 +1,82 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "ArduinoJson.h"
+
+void execHTTPcheck(String firwmaretype , int firwmareversion) {
+  
+  WiFiClient client;
+  String host ;
+  String bin ;
+  int port = 80;
+  
+   Serial.println("Getting HTTP");
+   Serial.println("----------------------");
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+ 
+    HTTPClient http;
+ 
+    http.begin(firwmareupdate); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+ 
+    if (httpCode == 200 ) { //Check is a file was returned
+ 
+        String payload = http.getString();
+
+        int str_len = payload.length() + 1; 
+        char JSONMessage[str_len];
+        payload.toCharArray(JSONMessage, str_len);
+        
+        StaticJsonBuffer<300> JSONBuffer;                         //Memory pool
+        JsonObject& parsed = JSONBuffer.parseObject(JSONMessage); //Parse message
+        
+        if (!parsed.success()) {   //Check for errors in parsing
+          Serial.println("Parsing failed");
+          delay(5000);
+          return;
+        }
+        
+        const char * pltype = parsed["type"];
+        int plversion = parsed["version"];
+        const char * plhost = parsed["host"];
+        port = parsed["port"];
+        const char * plbin = parsed["bin"];
+
+        String jshost(plhost);
+        String jsbin(plbin);
+
+        host = jshost;
+        bin = jsbin;
+        
+        String fwtype(pltype);
+        
+        if ( plversion > firwmareversion && fwtype == firwmaretype ) {
+          Serial.println("update needed");
+          Serial.print("Firmware type: ");
+          Serial.println(pltype);
+          Serial.print("version: ");
+          Serial.println(plversion);
+          
+          Serial.print("host : ");
+          Serial.println(host);
+          Serial.print("port : ");
+          Serial.println(port);
+          Serial.print("bin : ");
+          Serial.println(bin);
+
+          execOTA(client, host ,bin ,port);
+        } else {
+          Serial.println("no update needed");
+        }
+        
+      }
+ 
+    else {
+      Serial.println("Error on HTTP request");
+    }
+ 
+    http.end(); //Free the resources
+  }
+ 
+  Serial.println("----------------------");
+ 
+}
