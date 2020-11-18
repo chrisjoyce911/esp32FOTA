caa3411d5bc38996caeb6b7ea63df056f7e332d6
Adminius <eugenius707@gmail.com>
2020-11-18 23:19:30 +0100

    working https example

--- examples/HTTPS/HTTPS.ino
+++ examples/HTTPS/HTTPS.ino
@@ -35,56 +35,65 @@ const char* password = ""; // your network password
 
 char* test_root_ca= \
      "-----BEGIN CERTIFICATE-----\n"  
-  "MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\n"  
-  "ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\n"  
-  "b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\n"  
-  "MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n"  
-  "b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n"  
-  "ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n"  
-  "9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n"  
-  "IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n"  
-  "VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n"  
-  "93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n"  
-  "jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\n"  
-  "AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\n"  
-  "A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\n"  
-  "U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\n"  
-  "N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\n"  
-  "o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\n"  
-  "5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\n"  
-  "rqXRfboQnoZsG4q5WTP468SQvvG5\n"  
+  "MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBsMQswCQYDVQQG\n"  
+  "EwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMSsw\n"  
+  "KQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5jZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAw\n"  
+  "MFoXDTMxMTExMDAwMDAwMFowbDELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZ\n"  
+  "MBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFu\n"  
+  "Y2UgRVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm+9S75S0t\n"  
+  "Mqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTWPNt0OKRKzE0lgvdKpVMS\n"  
+  "OO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEMxChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3\n"  
+  "MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFBIk5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQ\n"  
+  "NAQTXKFx01p8VdteZOE3hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUe\n"  
+  "h10aUAsgEsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMB\n"  
+  "Af8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaAFLE+w2kD+L9HAdSY\n"  
+  "JhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3NecnzyIZgYIVyHbIUf4KmeqvxgydkAQ\n"  
+  "V8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6zeM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFp\n"  
+  "myPInngiK3BD41VHMWEZ71jFhS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkK\n"  
+  "mNEVX58Svnw2Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n"  
+  "vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep+OkuE6N36B9K\n" 
   "-----END CERTIFICATE-----\n" ;
   
 
+WiFiClientSecure clientForOta;
+secureEsp32FOTA secureEsp32FOTA("esp32-fota-https", 1);
+
 void setup(){
   Serial.begin(115200);
-  WiFiClientSecure clientForOta;
 
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   // attempt to connect to Wifi network:
+  byte retries = 0;
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     // wait 1 second for re-trying
     delay(1000);
+    retries++;
+    if((retries % 5) == 4) WiFi.begin(ssid, password); //solves AUTH_FAIL bug on some routers
   }
-  
-     secureEsp32FOTA secureEsp32FOTA("sensing-pot", 1);
-     secureEsp32FOTA._host="firmwareHostWithoutPrependedHTTT.com"; //eg example.com
-     secureEsp32FOTA._descriptionOfFirmwareURL="/path-to-firmware-description"; //e /my-fw-versions
-     secureEsp32FOTA._certificate=test_root_ca;
-     secureEsp32FOTA.clientForOta=clientForOta;
-    bool shouldExecuteFirmwareUpdate=secureEsp32FOTA.execHTTPSCheck();
+  Serial.println("");
+  Serial.print("Connected, IP: ");
+  Serial.println(WiFi.localIP());
+     
+  secureEsp32FOTA._host="raw.githubusercontent.com"; //e.g. example.com
+  secureEsp32FOTA._descriptionOfFirmwareURL="/chrisjoyce911/esp32FOTA/master/examples/HTTPS/fota/firmware.json"; //e.g. /my-fw-versions/firmware.json
+  secureEsp32FOTA._certificate=test_root_ca;
+  secureEsp32FOTA.clientForOta=clientForOta;
+
+  bool shouldExecuteFirmwareUpdate=secureEsp32FOTA.execHTTPSCheck();
   if(shouldExecuteFirmwareUpdate)
   {
-    secureEsp32FOTA._certificate=test_root_ca;
+    Serial.println("Firmware update available!");
     secureEsp32FOTA.executeOTA();
   }
-    
 }
 
 
 void loop()
 {
-  
-  }
+
+}
