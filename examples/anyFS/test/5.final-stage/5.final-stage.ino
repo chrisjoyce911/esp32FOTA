166aadec533b998cbe27c10a05354657a9737539
tobozo <tobozo@users.noreply.github.com>
2022-09-14 13:26:54 +0200

    Update 5.final-stage.ino

--- examples/anyFS/test/5.final-stage/5.final-stage.ino
+++ examples/anyFS/test/5.final-stage/5.final-stage.ino
@@ -3,7 +3,7 @@
 void setup()
 {
   Serial.begin( 115200 );
-  Serial.println("\n\n\n**************************\n\n\n  TEST SUITE COMPLETE\n\n\n");
+  Serial.println("\n\n\n**************************\n\n\nTEST SUITE COMPLETE\n\n\n");
 }
 
 void loop()
